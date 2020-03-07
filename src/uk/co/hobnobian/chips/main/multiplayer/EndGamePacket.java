7547d028e82ebd762431da2580ab483ef44f26c2
Ollie Sycamore <olliesycamore@gmail.com>
2020-03-07 17:11:03 +0000

    Create EndGamePacket.java

---
+++ src/uk/co/hobnobian/chips/main/multiplayer/EndGamePacket.java
@@ -0,0 +1,5 @@
+package uk.co.hobnobian.chips.main.multiplayer;
+
+import java.io.Serializable;
+
+public class EndGamePacket implements Serializable {private static final long serialVersionUID = -2314938317599982601L;}
